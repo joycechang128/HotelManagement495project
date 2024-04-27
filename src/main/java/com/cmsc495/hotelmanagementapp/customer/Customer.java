94661f724ccb800c31c1cab4ed80f108d35a11eb
joyce chang <joycechang128@gmail.com>
2024-04-27 23:13:14 +0800

    customer system data logics and data shown on frontend completed

--- src/main/java/com/cmsc495/hotelmanagementapp/customer/Customer.java
+++ src/main/java/com/cmsc495/hotelmanagementapp/customer/Customer.java
@@ -3,40 +3,74 @@ package com.cmsc495.hotelmanagementapp.customer;
  * File: Customer.java
  * Path: src/main/java/com/cmsc495/hotelmanagementapp/customer/Customer.java
  * Package: com.cmsc495.hotelmanagementapp.customer
- * Author: Mrigank Gupta
+ * Author: Mrigank Gupta, Chia-Yu(Joyce) Chang
  * Created: 2024-04-11
- * Last Modified: 2024-0x-xx
- * Description: This file contains...
- * 				...
+ * Last Modified: 2024-04-27 
+ * Description: This file contains the entity class that represents the a hotel customer.
+ *              It contains information such as customer id, name, email, phone number, 
+ *              and associated reservation and billing details.
  */
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.annotations.Formula;
+
+import com.cmsc495.hotelmanagementapp.billing.Billing;
+import com.cmsc495.hotelmanagementapp.reservation.Reservation;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "customer")
-public class Customer {
+public class Customer implements Serializable {
 	
-	@Id													// primary key of the database table
-	@GeneratedValue(strategy=GenerationType.IDENTITY)	// reservationId value is generated automatically by the database
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "CustomerID")
 	private int customerId;
 	
 	@Column(name = "CustomerName", nullable = false)
 	private String customerName;
 	
+	@Column(name = "Email", nullable = false)
+	private String customerEmail;
+	
+	@Column(name = "PhoneNumber", nullable = false)
+	private String customerPhoneNumber;
+	
+	@OneToMany(mappedBy = "customer")
+	private List<Reservation> reservations;
+	
+	@OneToMany(mappedBy = "customer")
+	private List<Billing> billings;
+	
+	@Formula(value = "(SELECT COUNT(*) FROM reservation r WHERE r.CustomerID = customerId)")
+	private int numOfReservations;
+	
+	@Transient
+	private String overallPaymentStatus;
+		
 	// constructor
 	public Customer() {}
-
-	public Customer(String customerName) {
+	
+	public Customer(String customerName, String customerEmail, String customerPhoneNumber, List<Reservation> reservations,
+			List<Billing> billings) {
 		super();
 		this.customerName = customerName;
+		this.customerEmail = customerEmail;
+		this.customerPhoneNumber = customerPhoneNumber;
+		this.reservations = reservations;
+		this.billings = billings;
 	}
-	
+
 	public int getCustomerId() {
 		return customerId;
 	}
@@ -44,4 +78,42 @@ public class Customer {
 	public String getCustomerName() {
 		return customerName;
 	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerEmail() {
+		return customerEmail;
+	}
+
+	public void setCustomerEmail(String customerEmail) {
+		this.customerEmail = customerEmail;
+	}
+
+	public String getCustomerPhoneNumber() {
+		return customerPhoneNumber;
+	}
+
+	public void setCustomerPhoneNumber(String customerPhoneNumber) {
+		this.customerPhoneNumber = customerPhoneNumber;
+	}
+
+	public List<Reservation> getReservations() {
+		return reservations;
+	}
+
+	public int getNumOfReservations() {
+        return numOfReservations;
+	}
+	
+	public List<Billing> getBillings() {
+		return billings;
+	}
+	
+	public String getOverallPaymentStatus() {
+		CustomerService customerService = new CustomerService();
+        overallPaymentStatus = customerService.getOverallPaymentStatus(this);
+        return overallPaymentStatus;
+    }
 }
--- src/main/java/com/cmsc495/hotelmanagementapp/customer/CustomerRepository.java
+++ src/main/java/com/cmsc495/hotelmanagementapp/customer/CustomerRepository.java
@@ -2,15 +2,16 @@ package com.cmsc495.hotelmanagementapp.customer;
 /*
  * File: CustomerRepository.java
  * Path: src/main/java/com/cmsc495/hotelmanagementapp/customer/CustomerRepository.java
- * Package: com.cmsc495.hotelmanagementapp.customer
- * Author: Mrigank Gupta
+  * Package: com.cmsc495.hotelmanagementapp.customer
+ * Author: Mrigank Gupta, Chia-Yu(Joyce) Chang
  * Created: 2024-04-11
- * Last Modified: 2024-0x-xx
- * Description: This file contains...
- * 				...
+ * Last Modified: 2024-04-27
+ * Description: This repository interface provides methods for accessing and manipulating data in the database related to customers. 
+ *              It defines CRUD operations for customer entities, allowing interaction with the underlying data store.
  */
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -18,5 +19,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer>  {
 
+	Customer save(Customer customer);
+	
+	Optional<Customer> findCustomerByCustomerId(int customerId);
+	
 	List<Customer> findAll();
+	
+	void deleteCustomerByCustomerId(int customerId);
+	
 }
